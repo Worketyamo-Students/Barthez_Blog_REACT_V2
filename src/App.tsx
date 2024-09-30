33bda0657f382ca35575554b418a93ac4a737056
01Barthez <kenwoubarthez@gmail.com>
2024-09-30 13:15:15 +0100

    ajout du formulaire d'inscription avec auto-form et zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,128 @@
 import React from 'react'
-import App2 from './App-Nav'
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"
+import PageError from './pages/PageError';
+import SingleBlog from './pages/SingleBlog';
+import Inscription from './pages/Inscription';
+import Connexion from './pages/Connexion';
+
+
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <>
+                <Outlet />
+            </>
+        ),
+        // Page erreur
+        errorElement: (
+            <PageError />
+        ),
+        children: [
+            // Page d'inscription
+            {
+                path: 'signup',
+                element: (
+                    <>
+                        <Inscription/>
+                    </>
+                )
+            },
+            // Page de connexion
+            {
+                path: 'login',
+                element: (
+                    <>
+                        <Connexion/>
+                    </>
+                )
+            },
+
+            // Page de verification d'otp
+            {
+                path: 'verify-otp',
+                element: (
+                    <>
+                        <h2>verify otp</h2>
+                    </>
+                )
+            },
+
+            // Page de Resend otp
+            {
+                path: 'resend-otp',
+                element: (
+                    <>
+                        <h2>resend otp</h2>
+                    </>
+                )
+            },
+
+            // ManageUser
+            {
+                path: '',
+                element: (
+                    <>
+                        <Outlet/>
+                    </>
+                ),
+                children: [
+                    // Get one user information
+                    {
+                        path: '/:userID/profile',
+                        element: (<>
+                            <h1>Here user Info</h1>
+                        </>)
+                    },
+
+                    // Update users
+                    {
+                        path: '/profile',
+                        element: (<>
+                            <h1>Here user Info setting</h1>
+                        </>)
+                    },
+
+                ]
+            },
+
+            // Managing Blogs
+            {
+                path: '',
+                element: (
+                    <>
+                        <Outlet />
+                    </>
+                ),
+                children: [
+                    {
+                        path: 'home',
+                        element: (
+                            <h2>Blog Home Page</h2>
+                        ),
+                    },
+
+                    {
+                        path: 'blog/:id',
+                        element: (
+                            <>
+                                <SingleBlog />
+                            </>
+                        )
+                    }
+                ]
+            },
+        ]
+    },
+])
 
 const App: React.FC = () => {
-  return (
-    <>
-      <App2/>
-    </>
-  )
+    return (
+        <div>
+            <RouterProvider router={Router} />
+        </div>
+    )
 }
 
 export default App
--- src/pages/Inscription.tsx
+++ src/pages/Inscription.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+"use client";
+import React from "react";
+import * as z from "zod";
+import AutoForm, { AutoFormSubmit } from "../components/ui/auto-form";
 
-const Inscription:  React.FC = () => {
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
+
+// Define your form schema using zod
+const formSchema = z.object({
+  username: z
+    .string({
+      required_error: "la saisie du nom est obligatoire !",
+      invalid_type_error: "le format du nom n'est pas valide"
+    })
+    .describe("Nom d'utilisateur !")
+    .trim()
+    .min(3, {
+      message: "votre nom n'est pas assez long !",
+    })
+    .max(50, {
+      message: "veillez réduire la taille de votre nom !"
+    }),
+
+  password: z
+    .string({
+      required_error: "Password is required.",
+    })
+    .trim()
+    .describe("Mot de passe")
+    .regex(passwordRegex, {
+      message: "Format du mot de passe invalide !"
+    })
+});
+
+const Inscription: React.FC = () => {
   return (
-    <div>Inscription</div>
-  )
+    <>
+      <AutoForm
+        formSchema={formSchema}
+      >
+        <AutoFormSubmit />
+      </AutoForm>
+    </>
+  );
 }
 
 export default Inscription
